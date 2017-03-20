b7dd6f08232e3de058f2648dd56ab07dc730e3e3
Cloudhax23 <qadeem@netsyms.com>
2017-03-19 20:53:54 -0500

    Create update.sql

---
+++ update.sql
@@ -0,0 +1,5 @@
+UPDATE (player) SET name = REPLACE(name, '"', '');
+UPDATE (clan) SET name = REPLACE(name, '"', '');
+UPDATE (account) SET name = REPLACE(name, '"', '');
+UPDATE (clan_map_marker) SET label = REPLACE(label, '"', '');
+UPDATE (territory) SET name = REPLACE(name, '"', '');
